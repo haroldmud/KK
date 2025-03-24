aadc4c57796e324f33a3bcefc6ebd4404a551f84
haroldmud <haroldmudosa@gmail.com>
2025-03-24 17:16:12 +0200

    feat: implementing the when expression for single variable

--- app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
+++ app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
@@ -3,19 +3,20 @@ package com.practice.rockpaperscissor
 fun main() {
 
     println("rock, paper, scissor ??? Enter your choice \uD83D\uDE0A \n")
-    val playerChoice = readln()
+    var playerChoice = readln()
     val randomNumber = (1..3).random()
     var computerChoice = ""
-    if (randomNumber == 1) {
-        computerChoice = "rock"
-    } else if (randomNumber == 2) {
-        computerChoice = "paper"
-    } else {
-        computerChoice = "scissor"
+    computerChoice = when (randomNumber) {
+        1 -> "rock"
+        2 -> "paper"
+        else -> "scissor"
     }
     print("computer chooses: $computerChoice \n\n")
     when {
-        (playerChoice == computerChoice) -> print("DRAW!!!!!...TRY AGAIN")
+        (playerChoice == computerChoice) -> {
+            print("DRAW!!!!!...TRY AGAIN, enter your choice");
+            playerChoice =  readln()
+        }
         (playerChoice == "rock" && computerChoice == "paper") -> print("You lost \uD83D\uDE14")
         (playerChoice == "paper" && computerChoice == "rock") -> print("you winn \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
         (playerChoice == "paper" && computerChoice == "scissor") -> print("You lost \uD83D\uDE14")
