a9f7c60170a9ea09d1b96a869354637ad3d9f340
haroldmud <haroldmudosa@gmail.com>
2025-03-24 16:10:59 +0200

    feat: testing the game

--- app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
+++ app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
@@ -3,7 +3,7 @@ package com.practice.rockpaperscissor
 fun main() {
 
     println("rock, paper, scissor ??? Enter your choice \uD83D\uDE0A \n")
-    var playerChoice = readln()
+    val playerChoice = readln()
     val randomNumber = (1..3).random()
     var computerChoice = ""
     if (randomNumber == 1) {
@@ -13,12 +13,15 @@ fun main() {
     } else {
         computerChoice = "scissor"
     }
-
+    print("computer chooses: $computerChoice \n\n")
     when {
         (playerChoice == computerChoice) -> print("DRAW!!!!!...TRY AGAIN")
         (playerChoice == "rock" && computerChoice == "paper") -> print("You lost \uD83D\uDE14")
         (playerChoice == "paper" && computerChoice == "rock") -> print("you winn \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
         (playerChoice == "paper" && computerChoice == "scissor") -> print("You lost \uD83D\uDE14")
-
+        (playerChoice == "rock" && computerChoice == "scissor") -> print("YOU ARE THE WINNER \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
+        (playerChoice == "scissor" && computerChoice == "rock") -> print("You lost \uD83D\uDE14")
+        (playerChoice == "scissor" && computerChoice == "paper") -> print("YOU ARE THE WINNER \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
+        else -> print("what was that...come on try again")
     }
 }
