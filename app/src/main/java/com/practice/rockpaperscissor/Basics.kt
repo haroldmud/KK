52e2c9761fdc97aaa77990339446efb418ac7b9c
haroldmud <haroldmudosa@gmail.com>
2025-03-24 15:14:15 +0200

    feat: learn about references and if...else statement

--- app/src/main/java/com/practice/rockpaperscissor/Basics.kt
+++ app/src/main/java/com/practice/rockpaperscissor/Basics.kt
@@ -1,5 +1,14 @@
 package com.practice.rockpaperscissor
 
 fun main() {
-    println("my name is harold \n I'm old enough to be dead\\ but special enough to live for ever")
+    print("Enter your age please\n")
+
+    val age = readln().toIntOrNull() ?: -1
+    if(age in 2..17) {
+        println("you ain't allowed to enter ")
+    } else if (age in 18..29) {
+        println("you can get in the first stage")
+    } else if (age < 1) {
+        println("not supported, TRY AGAIN...")
+    }
 }
