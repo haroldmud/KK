83f867ec5b13d0012ae108317512a0a50bc669b9
haroldmud <haroldmudosa@gmail.com>
2025-03-24 17:52:47 +0200

    feat: implement the do...while loop

--- app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
+++ app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
@@ -1,7 +1,8 @@
 package com.practice.rockpaperscissor
 
 fun main() {
-
+    var isPlayerTryingAgain: Boolean
+    do {
     println("rock, paper, scissor ??? Enter your choice \uD83D\uDE0A \n")
     var playerChoice = readln()
     val randomNumber = (1..3).random()
@@ -12,17 +13,40 @@ fun main() {
         else -> "scissor"
     }
     print("computer chooses: $computerChoice \n\n")
-    when {
-        (playerChoice == computerChoice) -> {
-            print("DRAW!!!!!...TRY AGAIN, enter your choice");
-            playerChoice =  readln()
+
+        when {
+            (playerChoice == computerChoice) -> {
+                print("DRAW!!!!!...TRY AGAIN \n");
+                isPlayerTryingAgain = true
+            }
+            (playerChoice == "rock" && computerChoice == "paper") -> {
+                print("You lost \uD83D\uDE14");
+                isPlayerTryingAgain = true
+            }
+            (playerChoice == "paper" && computerChoice == "rock") -> {
+                print("you winn \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
+                isPlayerTryingAgain = false
+            }
+            (playerChoice == "paper" && computerChoice == "scissor") -> {
+                print("You lost \uD83D\uDE14")
+                isPlayerTryingAgain = true
+            }
+            (playerChoice == "rock" && computerChoice == "scissor") -> {
+                print("YOU ARE THE WINNER \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
+                isPlayerTryingAgain = false
+            }
+            (playerChoice == "scissor" && computerChoice == "rock") -> {
+                print("You lost \uD83D\uDE14")
+                isPlayerTryingAgain = true
+            }
+            (playerChoice == "scissor" && computerChoice == "paper") -> {
+                print("YOU ARE THE WINNER \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
+                isPlayerTryingAgain = false
+            }
+            else -> {
+                print("what was that...come on try again")
+                isPlayerTryingAgain = true
+            }
         }
-        (playerChoice == "rock" && computerChoice == "paper") -> print("You lost \uD83D\uDE14")
-        (playerChoice == "paper" && computerChoice == "rock") -> print("you winn \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
-        (playerChoice == "paper" && computerChoice == "scissor") -> print("You lost \uD83D\uDE14")
-        (playerChoice == "rock" && computerChoice == "scissor") -> print("YOU ARE THE WINNER \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
-        (playerChoice == "scissor" && computerChoice == "rock") -> print("You lost \uD83D\uDE14")
-        (playerChoice == "scissor" && computerChoice == "paper") -> print("YOU ARE THE WINNER \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
-        else -> print("what was that...come on try again")
-    }
+    } while ( isPlayerTryingAgain )
 }
