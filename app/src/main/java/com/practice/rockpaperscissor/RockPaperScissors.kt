c31f213dac64d039ab9d5925f9903941411e622c
haroldmud <haroldmudosa@gmail.com>
2025-03-25 16:09:10 +0200

    feat: manipulating the parameters and functions in kotlin

--- app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
+++ app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
@@ -1,52 +1,13 @@
 package com.practice.rockpaperscissor
 
 fun main() {
-    var isPlayerTryingAgain: Boolean = true
-    while ( isPlayerTryingAgain ) {
-    println("rock, paper, scissor ??? Enter your choice \uD83D\uDE0A \n")
-    val playerChoice = readln()
-    val randomNumber = (1..3).random()
-    var computerChoice = ""
-    computerChoice = when (randomNumber) {
-        1 -> "rock"
-        2 -> "paper"
-        else -> "scissor"
-    }
-    print("computer chooses: $computerChoice \n\n")
+    makeCoffee(30)
+}
 
-        when {
-            (playerChoice == computerChoice) -> {
-                print("DRAW!!!!!...TRY AGAIN \n");
-                isPlayerTryingAgain = true
-            }
-            (playerChoice == "rock" && computerChoice == "paper") -> {
-                print("You lost \uD83D\uDE14");
-                isPlayerTryingAgain = true
-            }
-            (playerChoice == "paper" && computerChoice == "rock") -> {
-                print("you winn \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
-                isPlayerTryingAgain = false
-            }
-            (playerChoice == "paper" && computerChoice == "scissor") -> {
-                print("You lost \uD83D\uDE14")
-                isPlayerTryingAgain = true
-            }
-            (playerChoice == "rock" && computerChoice == "scissor") -> {
-                print("YOU ARE THE WINNER \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
-                isPlayerTryingAgain = false
-            }
-            (playerChoice == "scissor" && computerChoice == "rock") -> {
-                print("You lost \uD83D\uDE14")
-                isPlayerTryingAgain = true
-            }
-            (playerChoice == "scissor" && computerChoice == "paper") -> {
-                print("YOU ARE THE WINNER \uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89!!!")
-                isPlayerTryingAgain = false
-            }
-            else -> {
-                print("what was that...come on try again")
-                isPlayerTryingAgain = true
-            }
-        }
+fun makeCoffee(spoons: Int) {
+    var countSpoons = 0
+    while(countSpoons  in 0 .. spoons - 1 ) {
+        countSpoons ++
+        print("$countSpoons:we've added $spoons spoons of sugar \n")
     }
 }
