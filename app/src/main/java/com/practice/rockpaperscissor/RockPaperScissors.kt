82a379dfc3283af43e1b7ba8ed11d5e5f9c704e6
haroldmud <haroldmudosa@gmail.com>
2025-03-25 17:21:34 +0200

    feat: create a fully working coffee machine

--- app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
+++ app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
@@ -1,13 +1,42 @@
 package com.practice.rockpaperscissor
 
+
 fun main() {
-    makeCoffee(30)
+    println("please enter your name")
+    val coffeeFetcher: String = readln()
+    println("How many spoons of sugar you want us to add in your coffee")
+    val sugarSpoons: Int = readln().toInt()
+    makeCoffee(sugarSpoons, coffeeFetcher)
 }
 
-fun makeCoffee(spoons: Int) {
-    var countSpoons = 0
-    while(countSpoons  in 0..< spoons) { //this is the same as in 0 .. spoons - 1
-        countSpoons ++
-        print("$countSpoons:we've added $countSpoons ${if (countSpoons == 1 ) "spoon" else "spoons"} of sugar \n")
+fun makeCoffee(spoons: Int, name: String) {
+    var isDone = false
+    while (!isDone) {
+        var isExceed = false
+        val isConfirmed = readln()
+        when (spoons) {
+            in 1..<5 -> {
+                print("$name, we added $spoons ${if (spoons == 1) "spoon" else "spoons"} of sugar in your coffee\n")
+                isDone = true
+            }
+            in 5..10 -> {
+                isExceed = true
+                if(isConfirmed != "yes") {
+                    println("$name, Are you sure you want to add this much ? yes/no \n")
+                }
+            }
+            0 -> {
+                print("$name, your coffee is sugar free, Enjoy it")
+                isDone = true
+            }
+        }
+        if(isExceed && isConfirmed == "yes") {
+            isExceed = false
+            println("$name, We added $spoons of sugar in your coffee, but it's not good for your health")
+            isDone = true
+        } else if(!isExceed && isConfirmed !== "yes") {
+            println("$name, something went wrong")
+            isDone = true
+        }
     }
 }
