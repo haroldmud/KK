1b52a30be646f11f2d93a9c7cb9a7f738676125f
haroldmud <haroldmudosa@gmail.com>
2025-03-27 15:32:24 +0200

    feat: calling a function as an argument in another function

--- app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
+++ app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
@@ -2,11 +2,16 @@ package com.practice.rockpaperscissor
 
 
 fun main() {
-   val sum = handleSum("bozhe bomogi mne")
-    print(sum)
+   val sum = handleSum()
+    val texter = sendText()
+    print("$sum: $texter")
 }
 
-fun handleSum(txt: String?): Int {
+fun handleSum(txt: String? = sendText()): Int {
     val text = txt
     return text!!.length
+}
+
+fun sendText(): String {
+    return "it is just a russian song"
 }
