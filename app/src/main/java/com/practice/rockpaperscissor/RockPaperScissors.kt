e4fa53e6d6d879a8f15cf7eb667109cd16589705
haroldmud <haroldmudosa@gmail.com>
2025-03-28 09:27:27 +0200

    feat: learning about arrays\=

--- app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
+++ app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
@@ -2,13 +2,7 @@ package com.practice.rockpaperscissor
 
 
 fun main() {
-   myparentloop@ for(i in 10 downTo  -5 step 2) {
-       println("$i: loop me")
-       for( i in 1..3) {
-           println("***ok")
-           if(i == 2) {
-               break@myparentloop
-           }
-       }
-   }
+   val names: Array<String> = arrayOf("john", "miye")
+    names[1] = "Alain"
+    println(names.size)
 }
