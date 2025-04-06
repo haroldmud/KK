b999eeba09365dfa83e761a6cd602f7c21da6bac
haroldmud <haroldmudosa@gmail.com>
2025-04-06 13:51:21 +0200

    feat: implement small changes

--- app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
+++ app/src/main/java/com/practice/rockpaperscissor/RockPaperScissors.kt
@@ -1,13 +1,13 @@
 package com.practice.rockpaperscissor
 
 fun main() {
-   val names: Array<String> = arrayOf("john", "harold")
+   val names: Array<String> = arrayOf("john", "harold", "jonas", "jonzing")
     for (i in (names.size - 1).downTo(0)) {
         println(names[i])
     }
     var n = 0;
-    do {
+    while (n > names.size) {
         n++
         println(names[n])
-    } while (n > names.size)
+    }
 }
